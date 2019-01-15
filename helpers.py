2c223af365e5d6595bf4a8160de9d912a1cc818b
matthijs vollenbroek <matthijs.vollenbroek@gmail.com>
2019-01-15 13:23:02 +0000

    eerste functies

--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@ import urllib.request
 from cs50 import SQL
 from flask import redirect, render_template, request, session
 from functools import wraps
+from passlib.apps import custom_app_context as pwd_context
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 # nodig voor most_liked en most_disliked
 from operator import itemgetter
 # configure CS50 Library to use SQLite database
@@ -30,3 +32,16 @@ def most_disliked(user_id):
     post_dislikes = db.execute("SELECT total_dislikes from posts WHERE user_id = :user_id", user_id=user_id)
     newlist = sorted(post_dislikes, key=itemgetter('total_dislikes'))
     return newlist[0]['post_id']
+
+
+def register(user_name, password):
+    # check of gebruikersnaam al in gebruik is
+    match = db.execute("SELECT * FROM users WHERE username = :username", username=user_name)
+    if len(match) != 0:
+        return False
+    # wachtwoord encrypten
+    hash = pwd_context.hash(password)
+    db.execute("Insert INTO users (username, password) VALUES(:username, :password)", username=user_name, password=hash)
+    userid = db.execute("SELECT user_id FROM users WHERE username = :username", username=user_name)
+    session["user_id"] = userid[0]['id']
+    return redirect(url_for("homepage"))
