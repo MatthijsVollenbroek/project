65e130e6033b8129543847c313c02e915cbd28ab
matthijs vollenbroek <matthijs.vollenbroek@gmail.com>
2019-01-15 13:14:05 +0000

    eerste functies

--- helpers.py
+++ helpers.py
@@ -19,14 +19,14 @@ def login_required(f):
     return decorated_function
 
 def most_liked(user_id):
-    # check voor bepaalde user_id meest gelikete post
+    # returned voor bepaalde user_id meest gelikete post
     post_likes = db.execute("SELECT total_likes from posts WHERE user_id = :user_id", user_id=user_id)
     newlist = sorted(post_likes, key=itemgetter('total_likes'))
     return newlist[0]['post_id']
 
 
 def most_disliked(user_id):
-    # check voor bepaalde user_id meest gedislikete post
+    # returned voor bepaalde user_id meest gedislikete post
     post_dislikes = db.execute("SELECT total_dislikes from posts WHERE user_id = :user_id", user_id=user_id)
     newlist = sorted(post_dislikes, key=itemgetter('total_dislikes'))
     return newlist[0]['post_id']
